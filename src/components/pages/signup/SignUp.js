73bfca628b840f9a54b65d0d0a97d7bf71538271
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-07-15 23:48:46 +0100

    phone code input fix

--- src/components/pages/signup/SignUp.js
+++ src/components/pages/signup/SignUp.js
@@ -19,7 +19,11 @@ import {
   colors,
 } from "../../Syles/styles";
 import LogoIcon from "../../Logo/Logo";
-import { TextInput } from "../../pages/Formik/FormLib";
+import {
+  TextInput,
+  CodeTextInput,
+  PhoneNumberTextInput,
+} from "../../pages/Formik/FormLib";
 import applecircle from "../../Assets/applecirlce.png";
 import fbcircle from "../../Assets/fb-circle.png";
 import googlecircle from "../../Assets/googlecircle.png";
@@ -88,6 +92,7 @@ const SignUp = ({ user, createUser, ...props }) => {
               password: "",
               password_confirmation: "",
               username: "",
+              phone_code: "",
             }}
             onSubmit={handleSubmit}
             validationSchema={Yup.object({
@@ -110,11 +115,11 @@ const SignUp = ({ user, createUser, ...props }) => {
                 .required("confirm_password is Required")
                 .oneOf([Yup.ref("password"), "Passwords must match"]),
               username: Yup.string().required("Username is Required"),
+              phone_code: Yup.string().required("Phone Code is Required"),
             })}
           >
             {({ isSubmitting, values, handleBlur, handleChange }) => (
               <Form>
-                {/* <TextInput name="country" type="text" placeholder=" Country" /> */}
                 <CountryDropdown
                   style={{
                     width: "458px",
@@ -158,24 +163,22 @@ const SignUp = ({ user, createUser, ...props }) => {
                   type="text"
                   placeholder=" Username"
                 />
-                <TextInput type="tel" name="phone" placeholder="Phone Number" />{" "}
+                {/* <TextInput type="tel" name="phone" placeholder="Phone Number" />{" "} */}
                 <div
                   style={{
                     display: "flex",
-                    justifyContent: "space-around",
+                    justifyContent: "space-between",
                   }}
                 >
-                  {/* <FlagTextInput type="tel" name="flag" /> */}
+                  <CodeTextInput type="tel" name="phone_code">
+                    <option value="ngn">+ 234</option>
+                  </CodeTextInput>
 
-                  {/* <PhoneInput
-                  
-                    inputProps={{
-                      name: "phone",
-                    }}
-                  /> */}
-
-                  {/* <CodeTextInput type="tel" name="code" />
-                  <PhoneNumberTextInput type="tel" name="phone" /> */}
+                  <PhoneNumberTextInput
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone Number"
+                  />
                 </div>
                 <TextInput
                   type="text"
